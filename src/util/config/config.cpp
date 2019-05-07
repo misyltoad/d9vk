fb3f013381bdb035c91a8c738d7ef6e4700f4f42
Joshua Ashton <joshua@froggi.es>
2019-05-07 23:39:20 +0100

    [util] Disable strictPow for "A Hat in Time"
    Improves performance slightly.

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -150,6 +150,15 @@ namespace dxvk {
     { "NieRAutomata.exe", {{
       { "d3d11.constantBufferRangeCheck",   "True" },
     }} },
+
+    /**********************************************/
+    /* D3D9 GAMES                                 */
+    /**********************************************/
+
+    /* A Hat in Time                              */
+    { "HatinTimeGame.exe", {{
+      { "d3d9.strictPow",                   "False" },
+    }} },
   }};
 
 
