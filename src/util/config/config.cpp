a53d4f8d957007d9463034a1ca14623ca5dba6ad
Joshua Ashton <joshua@froggi.es>
2019-05-07 23:39:20 +0100

    [util] Disable strictPow for "A Hat in Time"
    Improves performance slightly.

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -135,6 +135,15 @@ namespace dxvk {
     { R"(\\HITMAN2\.exe$)", {{
       { "dxgi.customVendorId",              "10de" },
     }} },
+
+    /**********************************************/
+    /* D3D9 GAMES                                 */
+    /**********************************************/
+
+    /* A Hat in Time                              */
+    { "HatinTimeGame.exe", {{
+      { "d3d9.strictPow",                   "False" },
+    }} },
   }};
 
 
