0fb2669d49566745efb0782a44b56fc66ed0ada5
Robin <robin.kertels@outlook.com>
2019-05-25 21:05:07 +0200

    [d3d9] Add config option to allow read only locking

--- src/d3d9/d3d9_device.cpp
+++ src/d3d9/d3d9_device.cpp
@@ -3222,6 +3222,9 @@ namespace dxvk {
             DWORD                   Flags) {
     auto lock = LockDevice();
 
+    if (!m_d3d9Options.allowLockFlagReadonly)
+      Flags &= ~D3DLOCK_READONLY;
+
     UINT Subresource = pResource->CalcSubresource(Face, MipLevel);
     auto& desc = *(pResource->Desc());
 
@@ -3492,6 +3495,9 @@ namespace dxvk {
 
     if (unlikely(ppbData == nullptr))
       return D3DERR_INVALIDCALL;
+
+    if (!m_d3d9Options.allowLockFlagReadonly)
+      Flags &= ~D3DLOCK_READONLY;
 
     pResource->SetMapFlags(Flags);
 
