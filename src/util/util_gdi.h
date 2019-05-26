4f26f2b8a4b9d4d929fc07c6ec8ec18aad0e1fe9
Joshua Ashton <joshua@froggi.es>
2019-05-26 19:50:21 +0100

    [util] Look up ddi funcs in GDI using function ptrs
    MinGW doesn't keep these interfaces in its GDI lib

--- src/util/util_gdi.h
+++ src/util/util_gdi.h
@@ -2,29 +2,58 @@
 
 #include <d3d9.h>
 
-// Avoid redefinition if for some reason its here...
-using NTSTATUS_DXVK = LONG;
+namespace dxvk {
+  using NTSTATUS = LONG;
 
-// Slightly modified definitions...
-struct D3DKMT_CREATEDCFROMMEMORY {
-  void*         pMemory;
-  D3DFORMAT     Format;
-  UINT          Width;
-  UINT          Height;
-  UINT          Pitch;
-  HDC           hDeviceDc;
-  PALETTEENTRY* pColorTable;
-  HDC           hDc;
-  HANDLE        hBitmap;
-};
+  // Slightly modified definitions...
+  struct D3DKMT_CREATEDCFROMMEMORY {
+    void* pMemory;
+    D3DFORMAT     Format;
+    UINT          Width;
+    UINT          Height;
+    UINT          Pitch;
+    HDC           hDeviceDc;
+    PALETTEENTRY* pColorTable;
+    HDC           hDc;
+    HANDLE        hBitmap;
+  };
 
-struct D3DKMT_DESTROYDCFROMMEMORY {
-  HDC    hDC     = nullptr;
-  HANDLE hBitmap = nullptr;
-};
+  struct D3DKMT_DESTROYDCFROMMEMORY {
+    HDC    hDC = nullptr;
+    HANDLE hBitmap = nullptr;
+  };
 
-extern "C" NTSTATUS_DXVK STDMETHODCALLTYPE D3DKMTCreateDCFromMemory(
-        D3DKMT_CREATEDCFROMMEMORY* Arg1);
+  typedef NTSTATUS(STDMETHODCALLTYPE* D3DKMTCreateDCFromMemoryType) (D3DKMT_CREATEDCFROMMEMORY*);
+  typedef NTSTATUS(STDMETHODCALLTYPE* D3DKMTDestroyDCFromMemoryType)(D3DKMT_DESTROYDCFROMMEMORY*);
 
-extern "C" NTSTATUS_DXVK STDMETHODCALLTYPE D3DKMTDestroyDCFromMemory(
-        D3DKMT_DESTROYDCFROMMEMORY* Arg1);
+  inline NTSTATUS D3DKMTCreateDCFromMemory(D3DKMT_CREATEDCFROMMEMORY* Arg1) {
+    static D3DKMTCreateDCFromMemoryType D3DKMTCreateDCFromMemoryFunc = nullptr;
+
+    if (D3DKMTCreateDCFromMemoryFunc == nullptr) {
+      HMODULE gdi = LoadLibraryA("gdi32.dll");
+      D3DKMTCreateDCFromMemoryFunc =
+        (D3DKMTCreateDCFromMemoryType) GetProcAddress(gdi, "D3DKMTCreateDCFromMemory");
+    }
+
+    if (D3DKMTCreateDCFromMemoryFunc != nullptr)
+      return D3DKMTCreateDCFromMemoryFunc(Arg1);
+
+    return -1;
+  }
+
+  inline NTSTATUS D3DKMTDestroyDCFromMemory(D3DKMT_DESTROYDCFROMMEMORY* Arg1) {
+    static D3DKMTDestroyDCFromMemoryType D3DKMTDestroyDCFromMemoryFunc = nullptr;
+
+    if (D3DKMTDestroyDCFromMemoryFunc == nullptr) {
+      HMODULE gdi = LoadLibraryA("gdi32.dll");
+      D3DKMTDestroyDCFromMemoryFunc =
+        (D3DKMTDestroyDCFromMemoryType)GetProcAddress(gdi, "D3DKMTDestroyDCFromMemory");
+    }
+
+    if (D3DKMTDestroyDCFromMemoryFunc != nullptr)
+      return D3DKMTDestroyDCFromMemoryFunc(Arg1);
+
+    return -1;
+  }
+
+}
