b429db02adac42edcdc15573d12d2b0b31e88fb7
Joshua Ashton <joshua@froggi.es>
2019-06-11 18:37:33 +0100

    [util] Refactor Vector to be polymorphic

--- src/util/util_vector.h
+++ src/util/util_vector.h
@@ -6,57 +6,144 @@
 
 namespace dxvk {
 
-  struct Vector4 {
-    Vector4();
-    explicit Vector4(float splat);
-    Vector4(float x, float y, float z, float w);
-    Vector4(float xyzw[4]);
-    Vector4(const Vector4& other) = default;
+  template <typename T>
+  struct Vector4Base {
+    Vector4Base()
+      : x{ }, y{ }, z{ }, w{ } { }
+
+    Vector4Base(T splat)
+      : x(splat), y(splat), z(splat), w(splat) { }
+
+    Vector4Base(T x, T y, T z, T w)
+      : x(x), y(y), z(z), w(w) { }
+
+    Vector4Base(T xyzw[4])
+      : x(xyzw[0]), y(xyzw[1]), z(xyzw[2]), w(xyzw[3]) { }
+
+    Vector4Base(const Vector4Base<T>& other) = default;
 
     inline       float& operator[](size_t index)       { return data[index]; }
     inline const float& operator[](size_t index) const { return data[index]; }
 
-    bool operator==(const Vector4& other) const;
-    bool operator!=(const Vector4& other) const;
-	
-    Vector4 operator-() const;
+    bool operator==(const Vector4Base<T>& other) const {
+      for (uint32_t i = 0; i < 4; i++) {
+        if (data[i] != other.data[i])
+        return false;
+      }
 
-    Vector4 operator+(const Vector4& other) const;
+      return true;
+    }
 
-    Vector4 operator-(const Vector4& other) const;
+    bool operator!=(const Vector4Base<T>& other) const {
+      return !operator==(other);
+    }
 
-    Vector4 operator*(float scalar) const;
-    Vector4 operator*(const Vector4& other) const;
+    Vector4Base operator-() const { return {-x, -y, -z, -w}; }
 
-    Vector4 operator/(const Vector4& other) const;
-    Vector4 operator/(float scalar) const;
+    Vector4Base operator+(const Vector4Base<T>& other) const {
+      return {x + other.x, y + other.y, z + other.z, w + other.w};
+    }
 
-    Vector4& operator+=(const Vector4& other);
-    Vector4& operator-=(const Vector4& other);
-    Vector4& operator*=(float scalar);
-    Vector4& operator/=(float scalar);
+    Vector4Base operator-(const Vector4Base<T>& other) const {
+      return {x - other.x, y - other.y, z - other.z, w - other.w};
+    }
+
+    Vector4Base operator*(T scalar) const {
+      return {scalar * x, scalar * y, scalar * z, scalar * w};
+    }
+
+    Vector4Base operator*(const Vector4Base<T>& other) const {
+      Vector4Base result;
+      for (uint32_t i = 0; i < 4; i++)
+        result[i] = data[i] * other.data[i];
+      return result;
+    }
+
+    Vector4Base operator/(const Vector4Base<T>& other) const {
+      Vector4Base result;
+      for (uint32_t i = 0; i < 4; i++)
+        result[i] = data[i] / other.data[i];
+      return result;
+    }
+
+    Vector4Base operator/(T scalar) const {
+      return {x / scalar, y / scalar, z / scalar, w / scalar};
+    }
+
+    Vector4Base& operator+=(const Vector4Base<T>& other) {
+      x += other.x;
+      y += other.y;
+      z += other.z;
+      w += other.w;
+
+      return *this;
+    }
+
+    Vector4Base& operator-=(const Vector4Base<T>& other) {
+      x -= other.x;
+      y -= other.y;
+      z -= other.z;
+      w -= other.w;
+
+      return *this;
+    }
+
+    Vector4Base& operator*=(T scalar) {
+      x *= scalar;
+      y *= scalar;
+      z *= scalar;
+      w *= scalar;
+
+      return *this;
+    }
+
+    Vector4Base& operator/=(T scalar) {
+      x /= scalar;
+      y /= scalar;
+      z /= scalar;
+      w /= scalar;
+
+      return *this;
+    }
 
     union {
-      float data[4];
+      T data[4];
       struct {
-        float x, y, z, w;
+        T x, y, z, w;
       };
       struct {
-        float r, g, b, a;
+        T r, g, b, a;
       };
     };
 
   };
 
-  inline Vector4 operator*(float scalar, const Vector4& vector) {
+  template <typename T>
+  inline Vector4Base<T> operator*(T scalar, const Vector4Base<T>& vector) {
     return vector * scalar;
   }
 
-  float dot(const Vector4& a, const Vector4& b);
+  template <typename T>
+  float dot(const Vector4Base<T>& a, const Vector4Base<T>& b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+  }
 
-  float lengthSqr(const Vector4& a);
-  float length(const Vector4& a);
-  Vector4 normalize(const Vector4& a);
+  template <typename T>
+  T lengthSqr(const Vector4Base<T>& a) { return dot(a, a); }
+
+  template <typename T>
+  float length(const Vector4Base<T>& a) { return sqrtf(float(lengthSqr(a))); }
+
+  template <typename T>
+  Vector4Base<T> normalize(const Vector4Base<T>& a) { return a * T(1.0f / length(a)); }
+
+  template <typename T>
+  std::ostream& operator<<(std::ostream& os, const Vector4Base<T>& v) {
+    return os << "Vector4(" << v[0] << ", " << v[1] << ", " << v[2] << ", " << v[3] << ")";
+  }
+
+  using Vector4  = Vector4Base<float>;
+  using Vector4i = Vector4Base<int>;
 
   inline Vector4 replaceNaN(Vector4 a, float value) {
     for (uint32_t i = 0; i < 4; i++) {
@@ -70,7 +157,5 @@ namespace dxvk {
   inline Vector4 replaceNaN(Vector4 a) {
     return replaceNaN(a, 0.0f);
   }
-
-  std::ostream& operator<<(std::ostream& os, const Vector4& v);
 
 }
