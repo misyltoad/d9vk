173f80aa9e3d05b749b339d6a4e651bf4f3cf56e
Joshua Ashton <joshua@froggi.es>
2019-06-12 20:06:17 +0100

    [util] Cleanup GDI code

--- src/util/util_gdi.h
+++ src/util/util_gdi.h
@@ -7,7 +7,7 @@ namespace dxvk {
 
   // Slightly modified definitions...
   struct D3DKMT_CREATEDCFROMMEMORY {
-    void* pMemory;
+    void*         pMemory;
     D3DFORMAT     Format;
     UINT          Width;
     UINT          Height;
@@ -19,41 +19,14 @@ namespace dxvk {
   };
 
   struct D3DKMT_DESTROYDCFROMMEMORY {
-    HDC    hDC = nullptr;
+    HDC    hDC     = nullptr;
     HANDLE hBitmap = nullptr;
   };
 
   typedef NTSTATUS(STDMETHODCALLTYPE* D3DKMTCreateDCFromMemoryType) (D3DKMT_CREATEDCFROMMEMORY*);
+  NTSTATUS D3DKMTCreateDCFromMemory(D3DKMT_CREATEDCFROMMEMORY* Arg1);
+
   typedef NTSTATUS(STDMETHODCALLTYPE* D3DKMTDestroyDCFromMemoryType)(D3DKMT_DESTROYDCFROMMEMORY*);
-
-  inline NTSTATUS D3DKMTCreateDCFromMemory(D3DKMT_CREATEDCFROMMEMORY* Arg1) {
-    static D3DKMTCreateDCFromMemoryType D3DKMTCreateDCFromMemoryFunc = nullptr;
-
-    if (D3DKMTCreateDCFromMemoryFunc == nullptr) {
-      HMODULE gdi = LoadLibraryA("gdi32.dll");
-      D3DKMTCreateDCFromMemoryFunc =
-        (D3DKMTCreateDCFromMemoryType) GetProcAddress(gdi, "D3DKMTCreateDCFromMemory");
-    }
-
-    if (D3DKMTCreateDCFromMemoryFunc != nullptr)
-      return D3DKMTCreateDCFromMemoryFunc(Arg1);
-
-    return -1;
-  }
-
-  inline NTSTATUS D3DKMTDestroyDCFromMemory(D3DKMT_DESTROYDCFROMMEMORY* Arg1) {
-    static D3DKMTDestroyDCFromMemoryType D3DKMTDestroyDCFromMemoryFunc = nullptr;
-
-    if (D3DKMTDestroyDCFromMemoryFunc == nullptr) {
-      HMODULE gdi = LoadLibraryA("gdi32.dll");
-      D3DKMTDestroyDCFromMemoryFunc =
-        (D3DKMTDestroyDCFromMemoryType)GetProcAddress(gdi, "D3DKMTDestroyDCFromMemory");
-    }
-
-    if (D3DKMTDestroyDCFromMemoryFunc != nullptr)
-      return D3DKMTDestroyDCFromMemoryFunc(Arg1);
-
-    return -1;
-  }
+  NTSTATUS D3DKMTDestroyDCFromMemory(D3DKMT_DESTROYDCFROMMEMORY* Arg1);
 
 }
