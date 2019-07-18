66f0a3567abd4c0273f93b7e91c0d8a2eecd3117
Joshua Ashton <joshua@froggi.es>
2019-07-18 18:05:01 +0100

    [dxvk] Add helper to get size of memory/image in bytes

--- src/dxvk/dxvk_image.h
+++ src/dxvk/dxvk_image.h
@@ -260,6 +260,15 @@ namespace dxvk {
         result |= m_viewFormats[i] == format;
       return result;
     }
+
+    /**
+     * \brief Memory size
+     * 
+     * \returns The memory size of the image
+     */
+    VkDeviceSize memSize() const {
+      return m_memory.length();
+    }
     
   private:
     
