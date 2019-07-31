4f71560f259fea3b8be160344732313f8d33a0f3
Robin Kertels <robin.kertels@gmail.com>
2019-07-31 16:38:55 +0200

    [util] Disable readonly locking for Nostale

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -193,6 +193,10 @@ namespace dxvk {
     { "LOTDGame.exe", {{
       { "d3d9.hasHazards",                  "True" },
     }} },
+    /* Nostale                                    */
+    { "NostaleClientX.exe", {{
+      { "d3d9.allowLockFlagReadonly",       "False" },
+    }} },
   }};
 
 
