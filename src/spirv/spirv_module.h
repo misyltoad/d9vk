facd85c0bd863b71b22825d9cc12b16275fd62af
Joshua Ashton <joshua@froggi.es>
2019-09-09 15:58:50 +0100

    [spirv] Label operands for opReflect

--- src/spirv/spirv_module.h
+++ src/spirv/spirv_module.h
@@ -896,8 +896,8 @@ namespace dxvk {
 
     uint32_t opReflect(
             uint32_t                resultType,
-            uint32_t                a,
-            uint32_t                b);
+            uint32_t                incident,
+            uint32_t                normal);
 
     uint32_t opLength(
             uint32_t                resultType,
